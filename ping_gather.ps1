e353eb5100cf6dd9bfe2b560153009ae1915b603
Patrick Moon <patrickmoon@outlook.com>
2025-07-31 15:07:41 -0700

    we write csv not json so this fixes that.
    Also, fixed timestamp not being aligned to the timezone.
    Also, fixed $null on the wrong side.
    Also, fixed possible edge cases

--- ping_gather.ps1
+++ ping_gather.ps1
@@ -1,30 +1,29 @@
 ﻿<#
 .SYNOPSIS
-    ping_gather.ps1 - Pings IP address(es) and write the results to a JSON file.
+    ping_gather.ps1 - Pings IP address(es) and write the results to a CSV file.
 
 .DESCRIPTION
-    This script pings multiple IP addresses and writes the results to a JSON file. The script gets the latency value for an IP address and returns an object with the computer name, location IP, date and time, and actual latency value. It writes the results to a JSON file specified by $ResultFilePath.
+    This script pings multiple IP addresses and writes the results to a CSV file. The script gets the latency value for an IP address and returns an object with the computer name, location IP, date and time, and actual latency value. It writes the results to a CSV file specified by $ResultFilePath.
 
 .PARAMETER DestinationIPs
     An array of IP addresses to ping.
 
 .PARAMETER ResultFilePath
-    The path and filename of the JSON file to write the results to. If not specified, the default filename is "result_$env:computername_$((Get-Date).ToString('yyyyMMdd_HHmmss')).json".
+    The path and filename of the CSV file to write the results to. If not specified, the default filename is "result_$env:computername_$((Get-Date).ToString('yyyyMMdd_HHmmss')).csv".
 
 .EXAMPLE
-    .\ping_gather.ps1 "192.168.1.1","192.168.1.2","192.168.1.3" C:\Results.json
+    .\ping_gather.ps1 "192.168.1.1","192.168.1.2","192.168.1.3" C:\Results.csv
 
-    This example pings three IP addresses and writes the results to a JSON file located at "C:\Results.json".
+    This example pings three IP addresses and writes the results to a CSV file located at "C:\Results.csv".
 
 .EXAMPLE
     .\ping_gather.ps1 "192.168.1.1","192.168.1.2","192.168.1.3"
 
-    This example pings three IP addresses and writes the results to a JSON file located in the current directory with a default filename of "result_$env:computername_$((Get-Date).ToString('yyyyMMdd_HHmmss'))_{randomCharacters}.json".
+    This example pings three IP addresses and writes the results to a CSV file located in the current directory with a default filename of "result_$env:computername_$((Get-Date).ToString('yyyyMMdd_HHmmss'))_{randomCharacters}.json".
 
 .NOTES
     Author: Patrick Moon
 #>
-# version = 1.0
 param (
     [Parameter(Mandatory=$true)]
     [string[]]$DestinationIPs,
@@ -46,14 +45,15 @@ if( $ResultFilePath -eq "") {
     $ResultFilePath = $ResultFilePath -replace '\.csv$', "-$RandomString.csv"
 }
 Write-Host "Writing results to $ResultFilePath."
-   
 while ($true) {
-    $datetime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $datetime = Get-Date -Format "yyyy-MM-ddTHH:mm:ss"  # ISO 8601 format
+
+    $results = @()  # Collect results in an array
 
     foreach ($ip in $DestinationIPs) {
         $ping = Test-Connection -ComputerName $ip -Count 1 -ErrorAction SilentlyContinue
-        if ($ping -eq $null) {
-            $ping = New-Object PSObject -Property @{
+        if ($null -eq $ping) {
+            $pingObject = New-Object PSObject -Property @{
                 ComputerName = $ComputerName
                 Domain = $domain
                 LocationIP = $ip
@@ -61,9 +61,9 @@ while ($true) {
                 Latency = 1001
             }
         } else {
-            if ($ping.ResponseTime -eq $null) {
-                $wmi = Get-CimInstance -ClassName Win32_PingStatus -Filter "Address='$ip'"
-                if ($wmi.StatusCode -eq 0) {
+            if ($null -eq $ping.ResponseTime) {
+                $wmi = Get-CimInstance -ClassName Win32_PingStatus -Filter "Address='$ip'" -ErrorAction SilentlyContinue
+                if ($null -ne $wmi -and $wmi.StatusCode -eq 0) {
                     $pingObject = $ping | Select-Object @{Name='ComputerName';Expression={$ComputerName}}, @{Name='Domain';Expression={$domain}}, @{Name='LocationIP';Expression={$ip}}, @{Name='DateTime';Expression={$datetime}}, @{Name='Latency';Expression={$wmi.ResponseTime}}
                 } else {
                     $pingObject = New-Object PSObject -Property @{
@@ -78,8 +78,11 @@ while ($true) {
                 $pingObject = $ping | Select-Object @{Name='ComputerName';Expression={$ComputerName}}, @{Name='Domain';Expression={$domain}}, @{Name='LocationIP';Expression={$ip}}, @{Name='DateTime';Expression={$datetime}}, @{Name='Latency';Expression={$ping.ResponseTime}}
             }
         }
-        $pingObject | Export-Csv -Path $ResultFilePath -NoTypeInformation -Append
+        $results += $pingObject  # Add to results array
     }
+
+    # Write all results in one batch
+    $results | Export-Csv -Path $ResultFilePath -NoTypeInformation -Append
 
     Start-Sleep -Seconds 5
 }
